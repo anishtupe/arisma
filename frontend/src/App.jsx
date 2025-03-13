5404d3d0f2707a8751d89b5c702df63e6e5d21a5
Rizvi-Ahmed-Abbas-Nasir <abbasrizvi200209@gmail.com>
2025-03-13 18:26:53 +0530

    added tab title

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -35,7 +35,7 @@ const App = () => {
         cycleTitles();
       } else {
         clearTimeout(timeoutId);
-        document.title = "Home - My Website";
+        document.title = "ARISMA";
       }
     };
 
