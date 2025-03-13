7d5439f14bbf28799ebc42c0a42f475f96d760a3
Rizvi-Ahmed-Abbas-Nasir <abbasrizvi200209@gmail.com>
2025-03-13 18:25:06 +0530

    added tab title

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,50 @@ import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
 import Contact from './pages/Contact'
-import Product from './pages/product'
-import Cart from './pages/cart'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
 import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
-
+import { useEffect } from "react";
 
 
 const App = () => {
+
+
+  useEffect(() => {
+    let timeoutId;
+
+    const handleVisibilityChange = () => {
+      if (document.hidden) {
+        let titles = [
+          "Come back! We miss you 😢",
+          "Are you still there? 🧐",
+          "We have surprises for you! 🎁",
+        ];
+        let index = 0;
+
+        const cycleTitles = () => {
+          document.title = titles[index];
+          index = (index + 1) % titles.length;
+          timeoutId = setTimeout(cycleTitles, 2000); 
+        };
+
+        cycleTitles();
+      } else {
+        clearTimeout(timeoutId);
+        document.title = "Home - My Website";
+      }
+    };
+
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
 
